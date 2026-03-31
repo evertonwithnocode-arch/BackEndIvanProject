c37970f0c80672ac72b65641d535a08a872f6254
evertonwithnocode-arch <evertonwithnocode@gmail.com>
2026-03-31 11:44:20 -0300

    fix: adiciona middleware CORS para cloudflare

--- main.py
+++ main.py
@@ -18,8 +18,19 @@ from chromadb import Client
 from chromadb.config import Settings
 import shutil
 
-
 app = FastAPI()
+
+from fastapi.responses import Response
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response: Response = await call_next(request)
+
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+
+    return response
 
 
 # -------------------------------
@@ -32,7 +43,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # -------------------------------
 # CONFIG
 # -------------------------------
